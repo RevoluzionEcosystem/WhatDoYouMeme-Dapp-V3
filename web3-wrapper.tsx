289b4bc04a086a3ad623012584091b391a45a1f8
Nizar Syahmi bin Nazlan <38692430+nizarsyahmi37@users.noreply.github.com>
2024-01-20 22:32:13 +0800

    Add files via upload

---
+++ web3-wrapper.tsx
@@ -0,0 +1,46 @@
+"use client"
+
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
+import { WagmiConfig } from 'wagmi'
+import { bsc } from 'wagmi/chains'
+
+import info from "../data/lang/en/general.json"
+
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+const siteUrl = process.env.SITE_URL
+
+const chains = [ bsc ]
+const metadata = {
+    name: info.project_title,
+    description: info.project_description,
+    url: siteUrl,
+    icons: [
+        siteUrl + "assets/images/wdym.webp"
+    ]
+}
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+
+export default function Web3Wrapper({
+    children, mode
+}) {
+    createWeb3Modal({
+        wagmiConfig,
+        projectId,
+        chains,
+        themeMode: mode,
+        themeVariables: {
+            "--w3m-accent": "#09D1D8FF",
+            "--w3m-color-mix": "#28EFF6FF",
+            "--w3m-color-mix-strength": 15,
+            "--w3m-border-radius-master": "1px"
+        }
+    })
+    
+    return (
+        <div>
+            <WagmiConfig config={wagmiConfig}>
+                {children}
+            </WagmiConfig>
+        </div>
+    )
+}
